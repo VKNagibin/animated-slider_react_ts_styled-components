311bbd5639ef10f09bd8b857f62db228266059bf
v.nagibin <v.nagibin@worksolutions.ru>
2024-02-15 22:49:18 +0300

    add mock.ts

---
+++ src/components/App/mock.ts
@@ -0,0 +1,24 @@
+import { InitialSlideType } from './types'
+
+export const slides: InitialSlideType[] = [
+    {
+        img: 'https://mirpozitiva.ru/wp-content/uploads/2019/11/1477469601_nature_gora.jpg',
+        text: '1',
+    },
+    {
+        img: 'https://img.freepik.com/free-photo/river-surrounded-by-forests-under-a-cloudy-sky-in-thuringia-in-germany_181624-30863.jpg?w=2000',
+        text: '2',
+    },
+    {
+        img: 'https://bipbap.ru/wp-content/uploads/2021/08/1547365435_25.jpg',
+        text: '3',
+    },
+    {
+        img: 'https://mobimg.b-cdn.net/v3/fetch/90/905911947c2947e6dda5a8d9aa2af088.jpeg',
+        text: '4',
+    },
+    {
+        img: 'https://bipbap.ru/wp-content/uploads/2017/07/morskie_peyzagi_krasivie_plyagi_foto_12.jpg',
+        text: '5',
+    },
+]
